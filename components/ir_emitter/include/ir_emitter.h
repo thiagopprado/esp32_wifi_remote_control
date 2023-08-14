84bf73f8cde75a1a0792b0d70fc0d5989136b3a1
Thiago Prado <thiagopereiraprado@gmail.com>
2023-08-14 20:41:59 -0300

    Infrared sample code

--- components/ir_emitter/include/ir_emitter.h
+++ components/ir_emitter/include/ir_emitter.h
@@ -3,6 +3,14 @@
 
 #include <stdint.h>
 
+/** Samsung NEC codes */
+#define IR_NEC_SAMSUNG_ENTER    0xE0E016E9
+#define IR_NEC_SAMSUNG_ESC      0xE0E01AE5
+#define IR_NEC_SAMSUNG_UP       0xE0E006F9
+#define IR_NEC_SAMSUNG_DOWN     0xE0E08679
+#define IR_NEC_SAMSUNG_LEFT     0xE0E0A659
+#define IR_NEC_SAMSUNG_RIGHT    0xE0E046B9
+
 void ir_emitter_setup(void);
 void ir_emitter_nec(uint32_t value);
 
--- main/main.c
+++ main/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// FreeRTOS
+#include "freertos/FreeRTOSConfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+#include "freertos/timers.h"
+
 // ESP
 #include "esp_err.h"
 #include "esp_event.h"
@@ -8,13 +16,21 @@
 #include "esp_netif.h"
 #include "esp_err.h"
 
+#include "driver/gpio.h"
+
 #include "ir_emitter.h"
 
 void app_main(void)
 {
+    gpio_set_direction(GPIO_NUM_0, GPIO_MODE_INPUT);
+    gpio_pullup_en(GPIO_NUM_0);
+
     ir_emitter_setup();
 
     while (true) {
+        if (gpio_get_level(GPIO_NUM_0) == false) {
+            ir_emitter_nec(IR_NEC_SAMSUNG_ENTER);
+        }
 
         vTaskDelay(pdMS_TO_TICKS(100));
     }

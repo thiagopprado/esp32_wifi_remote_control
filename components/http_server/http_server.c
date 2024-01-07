782f0c25bb22afa208fc7626186b679a97736abc
Thiago Prado <thiagopereiraprado@gmail.com>
2024-01-07 20:02:47 -0300

    Embed favicon

--- components/http_server/http_server.c
+++ components/http_server/http_server.c
@@ -11,6 +11,7 @@
 #define HTTP_TAG    "HTTP"
 
 static esp_err_t get_page_handler(httpd_req_t *req);
+static esp_err_t favicon_handler(httpd_req_t *req);
 static esp_err_t process_command_handler(httpd_req_t *req);
 
 static http_command_callback_t command_callback = NULL;
@@ -19,6 +20,13 @@ static const httpd_uri_t page_uri = {
     .uri       = "/",
     .method    = HTTP_GET,
     .handler   = get_page_handler,
+    .user_ctx  = NULL,
+};
+
+static const httpd_uri_t favicon_uri = {
+    .uri       = "/favicon.ico",
+    .method    = HTTP_GET,
+    .handler   = favicon_handler,
     .user_ctx  = NULL,
 };
 
@@ -38,6 +46,19 @@ static esp_err_t get_page_handler(httpd_req_t *req)
     ESP_LOGI(HTTP_TAG, "GET page");
 
     httpd_resp_send(req, (const char *)webpage_start, webpage_size);
+
+    return ESP_OK;
+}
+
+static esp_err_t favicon_handler(httpd_req_t *req)
+{
+    extern const unsigned char favicon_start[] asm("_binary_favicon_ico_start");
+    extern const unsigned char favicon_end[] asm("_binary_favicon_ico_end");
+    const size_t favicon_size = (favicon_end - favicon_start);
+
+    ESP_LOGI(HTTP_TAG, "GET favicon");
+
+    httpd_resp_send(req, (const char *)favicon_start, favicon_size);
 
     return ESP_OK;
 }
@@ -73,6 +94,7 @@ void http_server_start(http_command_callback_t callback)
 
     if (httpd_start(&server, &config) == ESP_OK) {
         httpd_register_uri_handler(server, &page_uri);
+        httpd_register_uri_handler(server, &favicon_uri);
         httpd_register_uri_handler(server, &command_uri);
     }
 }
